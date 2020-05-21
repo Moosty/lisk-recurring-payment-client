da6e93a21e75b55e201740af962eacd2cf8c20b1
Corbifex <sander@moosty.com>
2020-05-21 22:43:52 +0200

    dayssssss

--- src/components/contract/ItemDetails.js
+++ src/components/contract/ItemDetails.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import './ItemDetails.less';
+import TimeAgo from 'react-time-ago';
+import { usePayments } from "../../hooks/payments";
 
 export const ContractItemDetails = (props) => {
-
+  const [
+    paymentsReady,
+    paymentsLeft,
+    nextPayment,
+    countdown,
+    now,
+    fundedUnits,
+  ] = usePayments(props.contract);
   switch (props.contract.asset.state) {
     case "SENDER_REVIEW":
       return (
@@ -32,11 +41,12 @@ export const ContractItemDetails = (props) => {
         </div>
       );
     case "ACTIVE":
+      const next = new Date(new Date().getTime() + ((nextPayment - now) * 1000));
       return (
         <div className="ContractItemDetailsContainer">
           <span className="ContractItemDetailsTitle">Contract: <b>{props.contract.asset.title}</b></span>
           {props.contract.asset.recipientPublicKey === props.publicKey ?
-            <span className="ContractItemDetailsSubTitle">Time to next request/request available</span> :
+            <span className="ContractItemDetailsSubTitle"><TimeAgo date={next}/></span> :
             <span className="ContractItemDetailsSubTitle">Contract is active</span>}
         </div>
       );
