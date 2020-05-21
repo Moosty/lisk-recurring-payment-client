d65e08b1aa01c1506d2d1335f70df91f21164e39
Corbifex <sander@moosty.com>
2020-05-21 22:34:59 +0200

    withdraw and review button

--- src/components/contract/boxes/index.js
+++ src/components/contract/boxes/index.js
@@ -3,3 +3,4 @@ export { DashboardBox } from './DashboardBox';
 export { HistoryBox } from './HistoryBox';
 export { HistoryItem } from './HistoryItem';
 export { ContractNotes } from './ContractNotes';
+export { ContractBoxItemActions } from './ContractBoxItemAction';
