372776314c39c2bb49440faa71605d1a18daec19
Corbifex <sander@moosty.com>
2020-05-23 14:32:32 +0200

    login fix

--- src/components/Login.js
+++ src/components/Login.js
@@ -36,6 +36,30 @@ const Login = ({passphrase, username, setPassphrase, setLogin, setUsername}) => 
 
   return (
     <div className="LoginContainer">
+      <div className="wrapper">
+      <svg className="triangle-canvas" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+        <polygon className="triangle triangle-1" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-2" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-3" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-4" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-5" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-6" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-7" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-8" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-9" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-10" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-11" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-12" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-13" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-14" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-15" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-16" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-17" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-18" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-19" points="500,200 759,650 241,650"/>
+        <polygon className="triangle triangle-20" points="500,200 759,650 241,650"/>
+      </svg>
+    </div>
       <div className="login">
 
         <h1>Login</h1>
@@ -85,30 +109,7 @@ const Login = ({passphrase, username, setPassphrase, setLogin, setUsername}) => 
             <Button disabled={!canLogin} onClick={setLogin}>Login</Button>
           </Form.Item>
         </Form>
-        <div className="wrapper">
-          <svg className="triangle-canvas" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
-            <polygon className="triangle triangle-1" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-2" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-3" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-4" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-5" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-6" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-7" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-8" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-9" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-10" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-11" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-12" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-13" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-14" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-15" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-16" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-17" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-18" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-19" points="500,200 759,650 241,650"/>
-            <polygon className="triangle triangle-20" points="500,200 759,650 241,650"/>
-          </svg>
-        </div>
+
       </div>
     </div>
   )
