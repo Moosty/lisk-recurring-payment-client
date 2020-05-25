c1d34574249c484ea98bf787881f9f6185643aa9
Corbifex <sander@moosty.com>
2020-05-25 15:49:23 +0200

    #10 mini help

--- src/components/About.js
+++ src/components/About.js
@@ -11,34 +11,37 @@ export const About = (props) => {
       <div className="About">
         <Typography>
           <Title>Recurring Payments</Title>
+          <Title level={2}>Overview</Title>
           <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam delectus ea laboriosam molestiae
-            voluptates.
-            Ad architecto <Text code>axure</Text> cumque cupiditate esse ipsam magni perferendis voluptas? Accusamus
-            accusantium, distinctio ipsum
-            iusto quae quam.
+            A list of transactions made by signed in wallet.
           </Paragraph>
+          <Title level={2}>Contracts</Title>
           <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam delectus ea laboriosam molestiae
-            voluptates.
-            Ad architecto <Text code>axure</Text> cumque cupiditate esse ipsam magni perferendis voluptas? Accusamus
-            accusantium, distinctio ipsum
-            iusto quae quam.
+            A list of contracts wallet is participating in.
           </Paragraph>
+          <Title level={2}>Create contract</Title>
           <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam delectus ea laboriosam molestiae
-            voluptates.
-            Ad architecto <Text code>axure</Text> cumque cupiditate esse ipsam magni perferendis voluptas? Accusamus
-            accusantium, distinctio ipsum
-            iusto quae quam.
+            With recurring payments 2 peers can construct a contract with recurring payment functionality. Every x amount
+            of time the recipient can collect the tokens for the previous period. Short fee is the fee that goes to the
+            recipient in case of early termination of the contract. Prepaid is the amount of payments needed before activation.
           </Paragraph>
-          <Title level={2}>Guidelines and Resources</Title>
+          <Title level={2}>Review</Title>
           <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam delectus ea laboriosam molestiae
-            voluptates.
-            Ad architecto <Text code>axure</Text> cumque cupiditate esse ipsam magni perferendis voluptas? Accusamus
-            accusantium, distinctio ipsum
-            iusto quae quam.
+            With the review button you can review the contract and either update or accept the contract. When updating the
+            contract the other party needs to review the contract.
+          </Paragraph>
+          <Title level={2}>Fund</Title>
+          <Paragraph>
+            With the fund button the sender of a contract can fund the contract for the recipient.
+          </Paragraph>
+          <Title level={2}>Withdraw</Title>
+          <Paragraph>
+            With the withdraw button the recipient of a contract can collect the available payments.
+          </Paragraph>
+          <Title level={2}>Terminate</Title>
+          <Paragraph>
+            With the terminate button the contract either on of the peers can terminate the contract after the prepaid time
+            is passed. A predefined short fee will be settle the remaining contract balance between the contract sender and recipient.
           </Paragraph>
         </Typography>
       </div>
